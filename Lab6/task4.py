cf8b928f341246660d3f720b64f5193fa7f09335
Stepan Smirnoff <foxelyss@gmail.com>
2024-10-04 10:55:36 +0700

    Адрессация вопросов и упрощение кода

--- Lab6/task4.py
+++ Lab6/task4.py
@@ -1,11 +1,4 @@
 a = int(input("Введите число а: "))
 b = int(input("Введите число б: "))
 
-
-def max_of_two(a, b):
-    if a > b:
-        return a
-    return b
-
-
-print(f"Максимальное из двух чисел: {max_of_two(a, b)}")
+print(f"Максимальное из двух чисел: {a if a > b else b}")
--- Lab6/task9.py
+++ Lab6/task9.py
@@ -1,13 +1,7 @@
 x = int(input("Введите число для поиска: "))
 array = list(map(int, input("Введите элементы массива через пробел: ").split()))
 
-element_found = False
-for i in array:
-    if x == i:
-        element_found = True
-        break
-
-if element_found:
+if x in array:
     print("Элемент имеется")
 else:
     print("Такого элемента нет")
