7ff884b53e7db5d8507240a394725c1923aec095
Stepan Smirnoff <foxelyss@gmail.com>
2024-10-04 10:10:40 +0700

    Правильный расчёт количества квадратов

--- Lab6/task3.py
+++ Lab6/task3.py
@@ -4,7 +4,7 @@ c = int(input("Введите сторону квадрата: "))
 
 
 def calculate_square_quantity_for_rectangle(width, length, cube_width):
-    return (width * length) // (cube_width ** 2)
+    return (width // cube_width) * (length // cube_width)
 
 
 print(f"Количество квадратов которых можно вырезать из этого прямоугольника = {calculate_square_quantity_for_rectangle(a, b, c)}")
