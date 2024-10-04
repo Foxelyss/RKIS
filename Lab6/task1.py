0cb5880f9c24658769b97ff536214e2ede69b01c
Stephan <foxelyss@gmail.com>
2024-10-04 13:08:53 +0700

    Упрощение 1-ого задания

--- Lab6/task1.py
+++ Lab6/task1.py
@@ -1,11 +1,5 @@
 import datetime
 
-days = ["Понедельник", "Вторник", "Среда",
-        "Четверг", "Пятница", "Суббота", "Воскресенье"]
-months = ["январь", "февраль", "март", "апрель", "май", "июнь",
-          "июль", "август", "сентябрь", "октябрь", "ноябрь", "декабрь"]
+today = datetime.datetime.today()
 
-month = datetime.datetime.today().month - 1
-day = datetime.datetime.today().weekday()
-
-print(f"Сегодня {days[day]}, {months[month]} месяц. Степан")
+print(f"Сегодня {today.strftime("%B %A")} месяц. Степан")
