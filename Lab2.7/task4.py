cb2265701eec597443bbfbcc80e3f0f9cda801b1
Stephan <foxelyss@gmail.com>
2025-06-06 11:45:31 +0700

    ПР 2.7 Задание 4

--- Lab2.7/task4.py
+++ Lab2.7/task4.py
@@ -19,13 +19,16 @@ clients = [
     FitnessCentreClient(1, 2023, 3, 321-14)
 ]
 
-years = {}
+minimum_duration_in_minutes_index = 0
+maximum_duration_in_minutes_index = 0
 
-for x in clients:
-    if x.year not in years:
-        years[x.year] = x.duration_in_minutes
-    else:
-        years[x.year] += x.duration_in_minutes
+for x in range(len(clients)):
+    if clients[x].duration_in_minutes < minimum_duration_in_minutes_index:
+        minimum_duration_in_minutes_index = x
+    elif clients[x].duration_in_minutes > maximum_duration_in_minutes_index:
+        maximum_duration_in_minutes_index = x
 
-
-
+minimum_duration_client = clients[minimum_duration_in_minutes_index]
+maximum_duration_client = clients[maximum_duration_in_minutes_index]
+print(f"FitnessCentreClient id:{minimum_duration_client.client_id} year: {minimum_duration_client.year} month: {minimum_duration_client.month} Minute duration: {minimum_duration_client.duration_in_minutes}")
+print(f"FitnessCentreClient id:{maximum_duration_client.client_id} year: {maximum_duration_client.year} month: {maximum_duration_client.month} Minute duration: {maximum_duration_client.duration_in_minutes}")
